ced2fc556640b317bfbe56907cd495aba1434d8c
Aarti <aarti.ranganathan1304@gmail.com>
2014-11-30 20:04:03 -0500

    second release

---
+++ crossword/crossword/clues.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace crossword
+{
+    partial class clues
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cluegrid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.cluegrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cluegrid
+            // 
+            this.cluegrid.AllowUserToAddRows = false;
+            this.cluegrid.AllowUserToDeleteRows = false;
+            this.cluegrid.AllowUserToResizeColumns = false;
+            this.cluegrid.AllowUserToResizeRows = false;
+            this.cluegrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.cluegrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.cluegrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cluegrid.Location = new System.Drawing.Point(0, 0);
+            this.cluegrid.Name = "cluegrid";
+            this.cluegrid.RowHeadersVisible = false;
+            this.cluegrid.Size = new System.Drawing.Size(384, 476);
+            this.cluegrid.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Number";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Directions";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Clues";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // clues
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 476);
+            this.Controls.Add(this.cluegrid);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "clues";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "clues";
+            ((System.ComponentModel.ISupportInitialize)(this.cluegrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        public System.Windows.Forms.DataGridView cluegrid;
+    }
+}
